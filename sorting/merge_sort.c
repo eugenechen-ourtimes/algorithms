7aa447c8b614f2340deb149a70e53b47464f39e0
eugenechen-ourtimes <eugene.chen19971108@gmail.com>
2024-02-18 21:14:59 +0800

    refactor merge sort

--- sorting/merge_sort.c
+++ sorting/merge_sort.c
@@ -2,66 +2,64 @@
 
 #include <stdlib.h>
 
-void _merge(int *a, int p, int q, int r, int *bl, int *br) {
-    int nl = q - p + 1;
-    int nr = r - q;
+void merge(int *a, int p, int q, int r, int *b_l, int *b_r) {
+    int n_l = q - p + 1;
+    int n_r = r - q;
     int i, j, k;
 
-    for (i = 0; i < nl; i++) {
-        bl[i] = a[p + i];
+    for (i = 0; i < n_l; i++) {
+        b_l[i] = a[p + i];
     }
 
-    for (j = 0; j < nr; j++) {
-        br[j] = a[q + 1 + j];
+    for (j = 0; j < n_r; j++) {
+        b_r[j] = a[q + 1 + j];
     }
 
     i = 0;
     j = 0;
     k = p;
-    while (i < nl || j < nr) {
-        if (i == nl) {
-            a[k] = br[j];
-            j++;
-            k++;
-            continue;
-        }
-
-        if (j == nr) {
-            a[k] = bl[i];
-            i++;
-            k++;
-            continue;
-        }
-
-        if (bl[i] < br[j]) {
-            a[k] = bl[i];
+    while (i < n_l && j < n_r) {
+        if (b_l[i] < b_r[j]) {
+            a[k] = b_l[i];
             i++;
             k++;
         } else {
-            a[k] = br[j];
+            a[k] = b_r[j];
             j++;
             k++;
-        }
+        }        
+    }
+
+    while (i < n_l) {
+        a[k] = b_l[i];
+        i++;
+        k++;
+    }
+
+    while (j < n_r) {
+        a[k] = b_r[j];
+        j++;
+        k++;
     }
 }
 
-void _mergeSort(int *a, int p, int r, int *bl, int *br) {
+void mergeSort(int *a, int p, int r, int *b_l, int *b_r) {
     if (p >= r) {
         return;
     }
 
     int q = (p + r) / 2;
-    _mergeSort(a, p, q, bl, br);
-    _mergeSort(a, q + 1, r, bl, br);
-    _merge(a, p, q, r, bl, br);
+    mergeSort(a, p, q, b_l, b_r);
+    mergeSort(a, q + 1, r, b_l, b_r);
+    merge(a, p, q, r, b_l, b_r);
 }
 
 void sort(int *a, int n) {
-    int *bl = (int *) malloc((n + 1) / 2 * sizeof(int));
-    int *br = (int *) malloc(n / 2 * sizeof(int));
+    int *b_l = (int *) malloc((n + 1) / 2 * sizeof(int));
+    int *b_r = (int *) malloc(n / 2 * sizeof(int));
 
-    _mergeSort(a, 0, n - 1, bl, br);
+    mergeSort(a, 0, n - 1, b_l, b_r);
 
-    free(bl);
-    free(br);
+    free(b_l);
+    free(b_r);
 }
